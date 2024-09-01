25ad2ac2770367f04028e52c093d297be76b4f80
Piotr Wicherski <pwicherski@gmail.com>
2024-09-02 00:25:38 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,8 @@
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: "8080",
-  },
-  base: '/exploratory-testing-app/',
   plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "lib",
-        replacement: resolve(__dirname, "lib"),
-      },
-    ],
-  },
-});
+  base: '/exploratory-testing-app/'
+})
