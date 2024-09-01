828f968a6c5af81a0b79e48466d613330accf625
Piotr Wicherski <pwicherski@gmail.com>
2024-09-02 00:12:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import SessionList from "./components/SessionList";
 import NoteTakingApp from "./pages/NoteTakingApp";
 
